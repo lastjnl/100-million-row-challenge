611eab42a52d50f412f47ae9b82731129569c4be
Jos Last <joslast96@gmail.com>
2026-03-03 08:05:32 +0100

    WIP

--- app/Parser.php
+++ app/Parser.php
@@ -2,12 +2,35 @@
 
 namespace App;
 
-use Exception;
-
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $handle = fopen($inputPath, 'r');
+        $data = [];
+        $json = "";
+       
+        while ($row = fscanf($handle, "%[^,],%[^\n]\n", $url, $time) !== false) {
+            $data[substr($url, 19, strlen($url))][] = substr($time, 0,10);
+        }
+
+        fclose($handle);
+
+        foreach ($data as $url => $dates) {
+            $countedDates = [];
+            foreach ($dates as $date => $count) {
+
+                if (isset($countedDates[$date]) === false) {
+                    $countedDates[$date] = 1;
+                } else {
+                    $countedDates[$date]++;
+                }
+            }
+            $data[$url] = $countedDates;
+        }
+
+        file_put_contents('test.json', json_encode($data));
+
+        $outputPath = json_encode($data);
     }
 }
