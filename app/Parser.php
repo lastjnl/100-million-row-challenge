e202486ca26ba4010f67bb798d67876d4a360c99
Jos Last <joslast96@gmail.com>
2026-03-09 19:58:21 +0100

    27sec

--- app/Parser.php
+++ app/Parser.php
@@ -2,35 +2,56 @@
 
 namespace App;
 
+use App\Commands\Visit;
+
 final class Parser
 {
+    private const CHUNK_SIZE = 100000;
+
     public function parse(string $inputPath, string $outputPath): void
     {
+        $buffer = '';
         $handle = fopen($inputPath, 'r');
+        stream_set_read_buffer($handle, 0);
+
+        $urlToId = [];
+        $visits = Visit::all();
         $data = [];
-        $json = "";
+        
+        foreach ($visits as $id => $visit) {
+            $sliced = substr($visit->uri, 19);
+            $urlToId[$sliced] = $id;
+        }
        
-        while ($row = fscanf($handle, "%[^,],%[^\n]\n", $url, $time) !== false) {
-            $data[substr($url, 19, strlen($url))][] = substr($time, 0,10);
+        while (feof($handle) === false) {
+            $buffer .= fread($handle, self::CHUNK_SIZE);
+            $lines = explode("\n", $buffer);
+            $buffer = array_pop($lines);
+            foreach ($lines as $line) {
+                $comma = strpos($line, ',');
+                $baseUrl = substr($line, 19, $comma - 19);
+                $date = substr($line, $comma + 1, 10);
+                $id = $urlToId[$baseUrl];
+                $data[$id][$date] = ($data[$id][$date] ?? 0) + 1;
+            }
+        }
+
+        if (!empty($buffer)) {
+            $comma = strpos($buffer, ',');
+            $id = $urlToId[substr($buffer, 19, $comma - 19)];
+            $date = substr($buffer, $comma + 1, 10);
+            $data[$id][$date] = ($data[$id][$date] ?? 0) + 1;
         }
 
         fclose($handle);
 
-        foreach ($data as $url => $dates) {
-            $countedDates = [];
-            foreach ($dates as $date => $count) {
-
-                if (isset($countedDates[$date]) === false) {
-                    $countedDates[$date] = 1;
-                } else {
-                    $countedDates[$date]++;
-                }
-            }
-            $data[$url] = $countedDates;
+        $handle = fopen($outputPath, 'w');
+        $output = [];
+        foreach ($data as $id => $timestamps) {
+            ksort($timestamps);
+            $output[substr($visits[$id]->uri, 19)] = $timestamps;
         }
-
-        file_put_contents('test.json', json_encode($data));
-
-        $outputPath = json_encode($data);
+        unset($timestamps);
+        fwrite($handle, json_encode($output, JSON_PRETTY_PRINT));
     }
 }
