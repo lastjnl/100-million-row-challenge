cdd65fbd9da0f2b2c1a6534265265eaa436c6af6
Jos Last <joslast96@gmail.com>
2026-03-10 20:43:13 +0100

    24sec (local time)

--- app/Parser.php
+++ app/Parser.php
@@ -2,8 +2,6 @@
 
 namespace App;
 
-use App\Commands\Visit;
-
 final class Parser
 {
     private const CHUNK_SIZE = 100000;
@@ -14,14 +12,7 @@ final class Parser
         $handle = fopen($inputPath, 'r');
         stream_set_read_buffer($handle, 0);
 
-        $urlToId = [];
-        $visits = Visit::all();
         $data = [];
-        
-        foreach ($visits as $id => $visit) {
-            $sliced = substr($visit->uri, 19);
-            $urlToId[$sliced] = $id;
-        }
        
         while (feof($handle) === false) {
             $buffer .= fread($handle, self::CHUNK_SIZE);
@@ -31,27 +22,53 @@ final class Parser
                 $comma = strpos($line, ',');
                 $baseUrl = substr($line, 19, $comma - 19);
                 $date = substr($line, $comma + 1, 10);
-                $id = $urlToId[$baseUrl];
-                $data[$id][$date] = ($data[$id][$date] ?? 0) + 1;
+                $data[$baseUrl][$date] = ($data[$baseUrl][$date] ?? 0) + 1;
             }
         }
 
         if (!empty($buffer)) {
             $comma = strpos($buffer, ',');
-            $id = $urlToId[substr($buffer, 19, $comma - 19)];
+            $baseUrl = substr($buffer, 19, $comma - 19);
             $date = substr($buffer, $comma + 1, 10);
-            $data[$id][$date] = ($data[$id][$date] ?? 0) + 1;
+            $data[$baseUrl][$date] = ($data[$baseUrl][$date] ?? 0) + 1;
         }
 
         fclose($handle);
 
-        $handle = fopen($outputPath, 'w');
-        $output = [];
-        foreach ($data as $id => $timestamps) {
+        $handle = fopen($outputPath, 'wb');
+        $firstUrl = true;
+
+        $dataKeys = array_keys($data);
+        $last_url = str_replace('/', '\/', end($dataKeys));
+
+        fwrite($handle, '{');
+
+        foreach ($data as $url => $timestamps) {
             ksort($timestamps);
-            $output[substr($visits[$id]->uri, 19)] = $timestamps;
+
+            if ($firstUrl) {
+                fwrite($handle, "\n");
+                $firstUrl = false;
+            }
+
+            $url = str_replace('/', '\/', $url);
+            $buffer = '    "' . $url . '": {' . "\n";
+
+            $timestampKeys = array_keys($timestamps);
+            $last_ts = end($timestampKeys);
+
+            foreach ($timestamps as $timestamp => $count) {
+                $buffer .= '        "' . $timestamp. '": ';
+                $buffer .= $count;
+                if ($timestamp !== $last_ts) $buffer .= ',';
+                $buffer .= "\n";
+            }
+
+            $buffer .= '    }';
+            if ($url !== $last_url) $buffer .= ',';
+            $buffer .=  "\n";
+            fwrite($handle, $buffer);
         }
-        unset($timestamps);
-        fwrite($handle, json_encode($output, JSON_PRETTY_PRINT));
+        fwrite($handle, '}');
     }
 }
